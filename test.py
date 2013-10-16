b0f87cdaa5516c6963e51d3a052d9b6fdc13b045
kim branson <kim@hessianinformatics.com>
2013-10-15 23:01:18 -0700

    added test

---
+++ test.py
@@ -0,0 +1,32 @@
+from popplerExtension import *
+P = PopplerParser("brian_test-2013-03-08.pdf")
+#print 'Number of pages ', P.getPages()
+print "-"*100 
+print "Starting Parse"
+results = P.Parse()
+print "#"*100
+print "FunctionResults"
+print results
+
+parseResults = []
+results = results.split("\n");
+for word in results:
+	word = word.split(",")
+	if len(word) > 0:
+		keys = []
+		vals = []
+		t = {}
+		for k in range(0, len(word), 2):
+			keys.append(word[k])
+		for v in range(1, len(word), 2):
+			vals.append(word[v])
+
+		for k, v  in zip(keys, vals):
+			print k, ' -> ',v
+			t[k] = v
+
+		print
+		parseResults.append(t)
+print "#"* 100
+print "done"
+print parseResults
