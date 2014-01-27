ff395ee51c9ed7a748334c47e642a9921a577c16
kim branson <kim@hessianinformatics.com>
2014-01-27 12:08:21 -0800

    added missing quote to the json output, updated test to not run 1000 times..

--- test.py
+++ test.py
@@ -1,24 +1,24 @@
 import json
 from popplerExtension import *
 
-for i in range(0, 1000):
 
-    P = PopplerParser("brian_test.pdf")
-	#P = PopplerParser("brian_test-2013-03-08.pdf")
-	#print 'Number of pages ', P.getPages()
-    print "-"*1 
-    print "Starting Parse " ,i
-    results = P.Parse()
-    print "#"*1
-    print "FunctionResults"
-    #print results
 
-    parseResults = []
-    results = results.split("\n");
-    for word in results:
-        if word:
-            word = json.loads(word)
-            parseResults.append(word)
-    print "#"* 1
-    print "done"
-    #print parseResults
+P = PopplerParser("brian_test.pdf")
+#P = PopplerParser("brian_test-2013-03-08.pdf")
+#print 'Number of pages ', P.getPages()
+print "-"*1 
+print "Starting Parse " 
+results = P.Parse()
+print "#"*1
+print "FunctionResults"
+#print results
+
+parseResults = []
+results = results.split("\n");
+for word in results:
+    if word:
+        word = json.loads(word)
+        parseResults.append(word)
+print "#"* 1
+print "done"
+print parseResults
